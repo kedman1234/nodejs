e78fa9deadfc3a457ea1e278193b8381a55177ee
Kedar Kulkarni <k.kulkarni@interproductec.com>
2018-06-11 22:51:19 +0530

    create basic server

---
+++ app.js
@@ -0,0 +1,11 @@
+var http = require('http');
+
+var server = http.createServer(function (request, response) {
+  // body...
+  response.writeHead(200, {"Content-Type": "text/plain"});
+  response.end("Hello World ");
+});
+
+server.listen(8000);
+
+console.log("server running at http://localhost:8000");
